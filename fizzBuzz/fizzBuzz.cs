59cc0623c28bb57aa2d947d7aa60a801a1e15575
Cyborg_Ean <ean.director@gmail.com>
2017-02-23 21:52:04 -0800

    practiced fizzBuzz

---
+++ fizzBuzz/fizzBuzz.cs
@@ -0,0 +1,35 @@
+/*
+    Write a program that prints the numbers 1 to 100.  But for multiples of three
+    print "Fizz" instead and for the multiples of five print "Buzz".  For numbers
+    which are multiples of both three and five print "FizzBuzz"
+*/
+
+﻿using System;
+namespace ConsoleApplication {
+    public class Program {
+
+        public static void fizzBuzz(int num){
+             for (int i = 0; i < num; i++){
+                 if (i % 3 == 0){
+                     if (i % 5 == 0){
+                         Console.WriteLine("FizzBuzz baybeh!!");
+                     }
+                     else {
+                         Console.WriteLine("Fizz");
+                     }
+                 }
+                 else if (i % 5 == 0){
+                     Console.WriteLine("Buzz");
+                 }
+                 else{
+                     Console.WriteLine(i);
+                 }
+             }
+        }
+
+        public static void Main(string[] args){
+            int cap = 100;
+            fizzBuzz(cap);
+        }
+    }
+}
