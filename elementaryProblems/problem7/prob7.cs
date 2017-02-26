d39a941733cb359b0271cfcd69fe05fa56376c87
Cyborg_Ean <ean.director@gmail.com>
2017-02-25 22:08:59 -0800

    making progress on exercise 7

--- elementaryProblems/problem7/prob7.cs
+++ elementaryProblems/problem7/prob7.cs
@@ -4,7 +4,43 @@
 
 using System;
 class Problem7{
+
+    public const int MAX = 4;
+    string table;
+
+    private int multiply(int root,int repition){
+        int result = 0;
+        for(int z = 0; z < repition; z++){
+            result += root;
+        }
+
+        return result;
+    }
+
+    private string renderInt(int num, int max){
+        string equation = Convert.ToString("");
+        for(int i = 0; i < max + 1; i++){
+            equation += String.Format("{0} x {1} = {2} | ",i ,num ,this.multiply(i,num) );
+        }
+        equation += "\n";
+
+        return equation;
+    }
+
+    public setTable(int num){
+        string tableRows = Convert.ToString("");
+        for(int r = 0;r < num; r++){
+            tableRows += this.renderInt(r,MAX);
+        }
+
+        this.table = tableRows;
+    }
+
     public static void Main(){
-        Console.WriteLine("Hello World! =D");
-   }
+        Problem7 app = new Problem7();
+        app.setTable(4);
+
+        Console.WriteLine(app.table);
+        //Console.WriteLine(app.multiply(9,9));
+    }
 }
