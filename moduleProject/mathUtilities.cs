3b3d78147f4a70829d3f44709aeed70034a723d9
Cyborg_Ean <ean.director@gmail.com>
2017-02-28 13:39:08 -0800

    Implementing queue for arbitrary argument assignments

--- moduleProject/mathUtilities.cs
+++ moduleProject/mathUtilities.cs
@@ -1,19 +1,39 @@
+/*
+    Create a multiplication method that can multiply an infinte number of
+    arguments recursively
+*/
+
 
 using System;
 class Multiplication {
 
     public static int calc(params int[] nums){
-        int root = nums[0];
-        int rep  = nums[1];
+        //result will is used to assign recursive function calls
+        int result;
+        //nextRep will check if next number is 0
+        int nextRep = 1;
 
-        if (rep == 0) {
-            return 0;
+        if (nums.Length < 3){
+            int root = nums[0];
+            int rep  = nums[1];
+            result = (rep == 0) ? 0 : root + calc(root, rep - 1);
         }
-        return root + calc(root, rep - 1);
+        else {
+            Console.WriteLine("Nums is too big!");
+            int queue = nums[nums.Length - 1];
+            int root = nums[0];
+            int rep = nums[queue];
+            result = (rep == 0)  ? 0 : root + calc(root, rep - 1);
+        }
+        return result;
     }
 
     public static void Main(string[] args){
-        int num = calc(3,3);
+        int[] ray = new int[5];
+        ray[0] = 3;
+        ray[1] = 3;
+        int num = calc(ray);
         Console.WriteLine("Hello World? {0}",num);
+        Console.WriteLine(ray.Length);
     }
 }
