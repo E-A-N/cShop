2e7fac6a5ae2523b1556f51d0b00c66d4d30745f
Cyborg_Ean <ean.director@gmail.com>
2017-03-03 12:34:46 -0800

    controlling structures by sanitizing inputs

--- moduleProject/mathUtilities.cs
+++ moduleProject/mathUtilities.cs
@@ -7,16 +7,36 @@
 using System;
 class Multiplication {
 
-    private void cloneArray(ref int[] ray1, ref int[] ray2, int iter = 0){
-        for(;iter < ray1.Length; iter++){
+    //Use data offset to adjust array for non-result appending items
+    private static int dataOffset = 2;
+
+    private static void cloneArray(ref int[] ray1, ref int[] ray2, int iter = 0){
+        for(;iter < ray1.Length - dataOffset; iter++){
             ray2[iter] = ray1[iter];
         }
+    }
+
+    private static int[] dataSantizer(params int[] nums){
+        int[] ray = new int[nums.Length + dataOffset];
+        cloneArray(nums,ray);
+        return ray;
     }
 
     public static int calc(params int[] nums){
         //result will is used to assign recursive function calls
         int result;
 
+        if (nums[0] == 0 || nums[1] == 0){
+            result = 0;
+        }
+        else if (nums[0] == 0 || nums[1] == 0){
+            result = 0;
+        }
+        else {
+            result = nums[0] + calc(nums[0],nums[1] - 1);
+        }
+
+        /*
         if (nums[nums.Length - 1] == 0){
             int sumAssign = nums.Length - 4; //total sum
             int repAssign = nums.Length - 3; //number of times to add iteration
@@ -46,6 +66,7 @@ class Multiplication {
             ray[ray.Length - 1] = 0;
             result = ray;
         }
+        */
         return result;
     }
 
