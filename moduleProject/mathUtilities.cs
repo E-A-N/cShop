95da5a70315ff9879e9426503600d76b3395e05a
Cyborg_Ean <ean.director@gmail.com>
2017-03-04 05:27:22 -0800

    Updating abstractions, adding structured logic

--- moduleProject/mathUtilities.cs
+++ moduleProject/mathUtilities.cs
@@ -10,19 +10,19 @@ using System;
 class Multiplication {
 
     //Use data offset to adjust array for non-result appending items
-    private const int dataOFFSET = 2;
-    private static void cloneArray(ref int[] ray1, ref int[] ray2){
+    private const int dataOFFSET = 5;
+    private static void cloneArray(ref int[] specimen, ref int[] clone){
         /*
             Clones array in place with the exception of offset values
-            managed within the dataSanitizing method
+            that are managed within the dataSanitizing function
         */
-        int relevantData = ray1.Length - dataOFFSET;
+        int relevantData = specimen.Length - dataOFFSET;
         for(int i = 0; i < relevantData; i++){
-            ray2[i] = ray1[i];
+            clone[i] = specimen[i];
         }
     }
 
-private static int[] dataSanitize(params int[] nums){
+    private static int[] dataSanitize(params int[] nums){
         /*
             This function appends neccesary arguments to an array of parameters
             to enable a solution using as few data types as possible.
@@ -30,19 +30,37 @@ private static int[] dataSanitize(params int[] nums){
             a solution recursively.
         */
         int[] ray = new int[nums.Length + dataOFFSET];
+        int size = ray.Length;
         cloneArray(ref nums,ref ray);
+        ray[size - 6] = 0;
+        ray[size - 5] = 0;
+        ray[size - 4] = 0;
+        ray[size - 3] = 0;
+        ray[size - 2] = 0;
+        ray[size - 1] = 0;
         return ray;
     }
 
     private static int calc(params int[] nums){
-        //result is used to assign recursive function calls
+        //result is used to assign recursive f unction calls
         int result;
+        int size = nums.Length;
+        int root = nums[size - 6]; //increment this value by rep
+        int rep = nums[size - 5];  //current number to multiply
+        int sum = nums[size - 4];  //number of times to add iteration
+        int inc = nums[size - 3]; //boolean flag 1 if incrementing else 0
+        int iter = nums[size - 2]; //current multiplication root
+        int bounds = nums[size - 1];
 
-        if (nums[0] == 0 || nums[1] == 0){
+        if (nums[root] == 0 || nums[rep] == 0){
             result = 0;
         }
-        else if (nums[0] == 0 || nums[1] == 0){
+        else if (nums[iter] < nums[bounds] && nums[rep] > 1){
+            //If iterations are before stopping point and repitions are left
+            nums[sum] += nums[root];
+
             result = 0;
+
         }
         else {
             result = nums[0] + calc(nums[0],nums[1] - 1);
